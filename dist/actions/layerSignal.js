90e5d76137ff9ef005099af39d5ec91351f7ea75
chukie-clt <chukie@lednets.com>
2024-11-01 18:14:06 +0800

    build: build release version

--- dist/actions/layerSignal.js
+++ dist/actions/layerSignal.js
@@ -1063,7 +1063,6 @@ function setupLayerSignalAction(context) {
             ];
             const actionSendBuf = Buffer.from(actionCommand);
             context.send(actionSendBuf);
-            log_1.logger.info(`actionBuf: ${actionSendBuf.toString('hex')}`);
             // 生效命令
             const efficientCommand = [0x52, 0x00, 0x10, 0x00, 0x00, 0x00, deviceIndexBuf[0], deviceIndexBuf[1], 0x00];
             const efficientSendBuf = Buffer.from(efficientCommand);
